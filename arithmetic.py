45e100936eddd73bd9d6ac362e90984b9cdb1cd4
Ahmad-Alawad <ahmad.alawad.sf@gmail.com>
2016-02-25 11:53:25 -0800

    Print statements added for all functions in arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,26 +1,34 @@
 def add(num1, num2):
+    print "in add function"
 	return num1+num2
 
 def subtract(num1, num2):
+    print "in subtract function"
     return num1 - num2
 
-def multiply(num1, num2):
+def multiply(num1, num2):    
+    print "in multiply function"
     return num1 * num2
 
 def divide(num1, num2):
+    print "in divide function"
 	if num2 != 0:
 		return num1 - num2
 	else:
 		return "Error"
 
 def square(num1):
+    print "in square function"
     return num1 * num1
 
 def cube(num1):
+    print "in cude function"
     return square(num1)*num1
 
 def power(num1, num2):
+    print "in power function"
     return pow(num1,num2)
 
 def mod(num1, num2):
+    print "in mod function"
     return num1 % num2
